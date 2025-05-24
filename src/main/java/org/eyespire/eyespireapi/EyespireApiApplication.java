45ec3ef05cde86130713c2edd9854e07deb36185
phuongtddd <phuongtdd3711@gmail.com>
2025-05-24 22:40:03 +0700

    first commit

---
+++ src/main/java/org/eyespire/eyespireapi/EyespireApiApplication.java
@@ -0,0 +1,13 @@
+package org.eyespire.eyespireapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyespireApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EyespireApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/eyespire/eyespireapi/EyespireApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eyespire.eyespireapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyespireApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
